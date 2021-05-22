3ee7f381658a6380ae9c8f0f21e9672e98e89422
Tom Fan <tomboxfan@hotmail.com>
2021-05-22 16:30:45 +0800

    update fangfang

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot and Sandy!";
+		return "Greetings from Spring Boot and FangFang!";
 	}
 
 }
